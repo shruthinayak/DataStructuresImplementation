c8024937a92d2074e2a203eb176ef41db44f35d3
shruthinayak <rn.shruthi@gmail.com>
2016-02-04 19:00:54 -0600

    Initial commit

---
+++ src/sp2/Vertex.java
@@ -0,0 +1,40 @@
+/**
+ * Class to represent a vertex of a graph
+ * 
+ *
+ */
+package sp2;
+
+import java.util.*;
+
+public class Vertex {
+	public int name; // name of the vertex
+	public boolean seen; // flag to check if the vertex has already been visited
+	public Vertex parent; // parent of the vertex
+	public int distance; // distance to the vertex from the source vertex
+	public List<Edge> Adj, revAdj; // adjacency list; use LinkedList or
+									// ArrayList
+	public int degree; // indegree in case of directed graph.
+
+	/**
+	 * Constructor for the vertex
+	 * 
+	 * @param n
+	 *            : int - name of the vertex
+	 */
+	Vertex(int n) {
+		name = n;
+		seen = false;
+		parent = null;
+		Adj = new ArrayList<Edge>();
+		revAdj = new ArrayList<Edge>(); /* only for directed graphs */
+		degree = n == 0 ? -1 : 0;
+	}
+
+	/**
+	 * Method to represent a vertex by its name
+	 */
+	public String toString() {
+		return Integer.toString(name);
+	}
+}
