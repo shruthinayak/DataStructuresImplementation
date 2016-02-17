c3c7f528c2081d3d65b150fcbd4d87e4daefedbf
shruthinayak <rn.shruthi@gmail.com>
2016-02-17 16:50:54 -0600

    LP0 merging done. Verify Euler Tour needs to be done.

--- src/lp0/Main.java
+++ src/lp0/Main.java
@@ -3,25 +3,77 @@ package lp0;
 import common.Utility;
 import sp2.Edge;
 import sp2.Graph;
-import sp2.GraphAlgorithms;
+import sp2.Vertex;
 
-import java.util.List;
+import java.util.*;
 
-/**
- * Created by shruthi on 12/2/16.
- */
 public class Main {
-    public static void main(String[] args) {
-        //String path = "/home/shruthi/AllFiles/OneDrive/Sem4/Impl/lp0-big.txt";
-        String path = "edges.txt";
-        Graph g = Utility.getGraph(path);
-        if (GraphAlgorithms.testEulerian(g)) {
 
-        }
+    public static int numEdges;
+
+    public static void main(String[] args) {
+        String path = "/home/shruthi/AllFiles/OneDrive/Sem4/Impl/lp0-big.txt"; //took 57758ms
+//        String path = "/home/shruthi/AllFiles/OneDrive/Sem4/Impl/lp0-data/tour/lp0-s1-ck.txt";
+//        String path = "edges.txt";
+
+        Graph g = Utility.getGraph(path);
+        System.out.println("Graph read");
+
+        long start = System.currentTimeMillis();
+        Node eulerTour = findEulerTour(g);
+        long end = System.currentTimeMillis();
+        System.out.println(eulerTour.size == numEdges);
+        System.out.println(end - start + "ms");
     }
 
-    static List<Edge> findEulerTour(Graph g) {
-//        List<Vertex>
-        return null;
+    static Node findEulerTour(Graph g) {
+        HashMap<Vertex, Node> hm = new HashMap<>();
+        Node root = new Node();
+        Set<Vertex> q = new LinkedHashSet<>();
+        Vertex masterSrc = pickRandomVertex(g.verts);
+        q.add(masterSrc);
+        hm.put(masterSrc, root);
+        while (!q.isEmpty() && root.size != numEdges) {
+            Vertex src = q.iterator().next();
+            q.remove(src);
+            Vertex next = src;
+            Node mergeNode = hm.get(next);
+            Node nextLink = mergeNode.link;
+            do {
+                if (next.degree > 0) {
+                    Edge edge = next.Adj.get(0);
+                    next.Adj.remove(0);
+                    next.degree--;
+                    next = edge.otherEnd(next);
+                    next.Adj.remove(edge);
+                    next.degree--;
+                    edge.now = next;
+                    mergeNode.link = new Node(edge);
+                    mergeNode = mergeNode.link;
+                    root.size++;
+                    hm.put(next, mergeNode);
+                    q.add(next);
+                } else {
+                    q.remove(next);
+                }
+            } while (next != src && root.link.size != numEdges);
+
+            if (nextLink != null)
+                mergeNode.link = nextLink;
+        }
+        return root;
+    }
+    /*static boolean verifyTour(Graph g, Node tour, Vertex start){
+
+    }*/
+
+    private static Vertex pickRandomVertex(List<Vertex> verts) {
+        if (verts.size() != 1) {
+            Random random = new Random();
+            int num = random.nextInt(verts.size() - 1);
+            return verts.get(num + 1);
+        }
+        return verts.get(0);
+
     }
 }
---
+++ src/lp0/Node.java
@@ -0,0 +1,28 @@
+package lp0;
+
+import sp2.Edge;
+
+/**
+ * Created by shruthi on 17/2/16.
+ */
+public class Node {
+    Edge edge;
+    Node link;
+    int size;
+
+    Node(Edge edge) {
+        this.edge = edge;
+    }
+
+    Node() {
+
+    }
+
+    void print() {
+        Node t = this;
+        while (t != null) {
+            System.out.println(t.edge.toString());
+            t = t.link;
+        }
+    }
+}
