0d7953237d887118e5ef65c4cc7c511c5b88cb57
Tejasvee Bolisetty <tbolisetty@gmail.com>
2016-02-24 17:49:39 -0600

    quick sort partition using dual pivot

--- src/sp3/MultipivotQuicksort.java
+++ src/sp3/MultipivotQuicksort.java
@@ -11,13 +11,23 @@ public class MultipivotQuicksort {
 
     public static void main(String args[]) {
 
-        int[] A = {1, 5, 2, 2, 4, 9, 2, 2, 7, 2};
-        quickSort(A, 0, A.length - 1);
+        int[] A = {1, 5, 2, 2, 4, 9, 2, 2, 7, 2, 4, 4, 6, 1, 0, -10};
+//        quickSort(A, 0, A.length - 1);
+        dualPivotSort(A, 0, A.length - 1);
+
         System.out.println(Arrays.toString(A));
     }
 
-    public static void quickSort(int[] A, int start, int end) {
+    private static void dualPivotSort(int[] A, int start, int end) {
+        int[] portion = dualPivotPartition(A, start, end);
+        if (portion != null) {
+            dualPivotSort(A, start, portion[0] - 1);
+            if (portion[2] != 1) dualPivotSort(A, portion[0] + 1, portion[1]);
+            dualPivotSort(A, portion[1], end);
+        }
+    }
 
+    public static void quickSort(int[] A, int start, int end) {
         if (start < end) {
             int q = partition1(A, start, end);
             quickSort(A, start, q - 1);
@@ -32,6 +42,71 @@ public class MultipivotQuicksort {
     }
 
 
+    public static int[] dualPivotPartition(int[] A, int start, int end) {
+        if (start >= end) return null;
+        int p = getRandom(start, end) + start;
+        int q = getRandom(start, end) + start;
+//        do{
+//            q=getRandom(start,end)+start;
+//        }while(p==q);
+        swap(A, p, start);
+        swap(A, q, end);
+//        p=2;
+//        q=3;
+        if (A[start] > A[end]) {
+            swap(A, start, end);
+        }
+        int x1 = A[start];
+        int x2 = A[end];
+        int i = start + 1;
+        int l = start + 1;
+        int j = end - 1;
+        while (i <= j) {
+            //case 1
+            // x1<= A[i] <=x2
+            if (x1 <= A[i] && A[i] <= x2) {
+                i++;
+            }
+            //case 2
+            //A[i] < x1
+            if (A[i] < x1) {
+                swap(A, l, i);
+                l++;
+                i++;
+            }
+            //case 3
+            //A[j]> x2
+            //j--
+            if (A[j] > x2) {
+                j--;
+            }
+
+            //case 4
+            //A[i] >x2 and A[j]<x1
+            // 3 way swap
+            if (A[i] > x2 && A[j] < x1) {
+                swap(A, i, j);
+                j--;
+                swap(A, i, l);
+                l++;
+                i++;
+            }
+            //case 5
+            //A[i]>x2 and x1<=A[j]<=x2
+            //swap i and j
+            if (A[i] > x2 && A[j] <= x2 && A[j] >= x1) {
+                swap(A, i, j);
+                i++;
+                j--;
+            }
+        }
+        swap(A, start, l - 1);
+        swap(A, end, j + 1);
+        int flag = 0;
+        if (x1 == x2) flag = 1;
+        int[] portions = {l - 1, i, flag};
+        return portions;
+    }
     public static int partition1(int[] A, int start, int end) {
         int q = getRandom(start, end) + start;
         swap(A, q, end);
@@ -48,7 +123,7 @@ public class MultipivotQuicksort {
         return i + 1;
     }
     public static int partition(int[] A, int start, int end) {
-        int pivot = getRandom(start, end);
+        int pivot = getRandom(start, end) + start;
         int i = start;
         int j = end;
         while (true) {
