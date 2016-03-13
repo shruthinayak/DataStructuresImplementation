dbeadef7bf8b5ee89fddaecba1baa2d8dde6a21d
Tejasvee Bolisetty <tbolisetty@gmail.com>
2016-03-13 00:00:01 -0600

    small changes for reading graph

--- src/lp2/GraphUtility.java
+++ src/lp2/GraphUtility.java
@@ -4,6 +4,7 @@ import sp2.Edge;
 import sp2.Graph;
 import sp2.Vertex;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
  * Created by shruthi on 5/3/16.
  */
 public class GraphUtility {
+    static HashSet<String> edges = new HashSet<>();
     static void DFS(Vertex v, List<Vertex> s, List<Vertex> output,
                     boolean useReverse, int component, boolean print) {
 
@@ -24,8 +26,11 @@ public class GraphUtility {
                 e.component = component;
                 Vertex u = e.otherEnd(v);
                 if (!u.seen && !u.strong) {
-                    if (print)
+                    if (print) {
+                        edges.add(e.toString());
+
                         System.out.println(e.toString());
+                    }
                     DFS(u, s, output, useReverse, component, print);
 
                 }
