6934531b6b7a5d641bf6beb49d3500d717687388
shruthinayak <rn.shruthi@gmail.com>
2016-04-09 20:44:29 -0500

    lp4 - Item POJO

---
+++ src/lp4/Item.java
@@ -0,0 +1,40 @@
+package lp4;
+
+/**
+ * Created by shruthi on 9/4/16.
+ */
+public class Item {
+    long itemId;
+    long[] description;
+    long price;
+
+    public Item(long itemId, long[] description, double price) {
+        this.itemId = itemId;
+        this.description = description;
+        this.price = price;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public long[] getDescription() {
+        return description;
+    }
+
+    public void setDescription(long[] description) {
+        this.description = description;
+    }
+
+    public long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(long itemId) {
+        this.itemId = itemId;
+    }
+}
