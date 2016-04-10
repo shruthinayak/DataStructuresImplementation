77a6c5815e11f8fecaf3493886c432048b796d99
shruthinayak <rn.shruthi@gmail.com>
2016-04-09 20:36:04 -0500

    lp4 - init commit

---
+++ src/lp4/MDS.java
@@ -0,0 +1,41 @@
+package lp4;
+
+public class MDS {
+    int insert(long id, double price, long[] description, int size) {
+        // Description of item is in description[0..size-1].
+        // Copy them into your data structure.
+        return 0;
+    }
+
+    double find(long id) {
+        return 0;
+    }
+
+    long delete(long id) {
+        return 0;
+    }
+
+    double findMinPrice(long des) {
+        return 0;
+    }
+
+    double findMaxPrice(long des) {
+        return 0;
+    }
+
+    int findPriceRange(long des, double lowPrice, double highPrice) {
+        return 0;
+    }
+
+    double priceHike(long minid, long maxid, double rate) {
+        return 0;
+    }
+
+    int range(double lowPrice, double highPrice) {
+        return 0;
+    }
+
+    int samesame() {
+        return 0;
+    }
+}
